0edd3eb74ca6830c31f32171529cd20bd262fe95
ruh0n <wns8256@naver.com>
2024-04-07 15:39:37 +0900

    :sparkles: feat(auth): implement login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Login from './pages/auth/Login.tsx';
@@ -6,17 +6,14 @@ import Home from './pages/home.tsx';
 import './App.css';
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const isLogin = useSelector((state) => state.user.isLogin);
 
   const navigate = useNavigate();
   useEffect(() => {
-    if (isLogin) {
-      setIsLoggedIn(true);
-    } else {
+    if (!isLogin) {
       navigate('/login');
     }
-  }, [isLogin]);
+  }, [isLogin, navigate]);
 
   return (
     <Routes>
