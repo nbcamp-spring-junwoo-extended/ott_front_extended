9cec5a7b2a9b4d644006e5c2b52d271a1e46b54b
gloz0315 <qudtn0315@naver.com>
2024-04-13 19:01:13 +0900

    :adhesive_bandage: fix: 프론트 수정

--- src/core/apis/videoApi.ts
+++ src/core/apis/videoApi.ts
@@ -1,16 +1,16 @@
 import { apiClient } from '../di/client.ts';
 
 export const getSearchComplete = (title: string) =>
-  apiClient.get('/api/v1/complete/search', {
+  apiClient.get('/api/v2/complete/search', {
     params: {
-      title,
+      input : title,
     },
   });
-Promise.resolve({ data: [] });
+// Promise.resolve({ data: [] });
 
 export const searchVideos = (searchOption: string, title: string) =>
-  apiClient.get('/api/v1/videos/search', {
+  apiClient.get('/api/v2/videos/search', {
     params: {
-      title,
+      input : title,
     },
   });
--- src/pages/search/components/VideoSearchBar.tsx
+++ src/pages/search/components/VideoSearchBar.tsx
@@ -1,10 +1,11 @@
-import { AutoComplete, Select, Space } from 'antd';
+import {AutoComplete, Select, Space} from 'antd';
 import Search from 'antd/es/input/Search';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
-import { getSearchComplete, searchVideos } from '../../../core/apis/videoApi.ts';
-import { VideoSearchResult } from '../../../core/types/video';
-import { useDebounce } from '../../../hooks/useDebounce.ts';
+import {getSearchComplete, searchVideos} from '../../../core/apis/videoApi.ts';
+import {VideoSearchResult} from '../../../core/types/video';
+import {useDebounce} from '../../../hooks/useDebounce.ts';
+import {isRouteErrorResponse} from "react-router-dom";
 
 export interface VideoSearchBarProps {
   setSearchResults: (value: VideoSearchResult) => void;
@@ -42,34 +43,37 @@ const fakeAutoComplete = [
   },
 ];
 
-const SelectSearchType: React.FC<SelectSearchTypeProps> = ({ setSelectedSearchOption }) => {
+const SelectSearchType: React.FC<SelectSearchTypeProps> = ({setSelectedSearchOption}) => {
   const searchOptions = [
-    { label: '제목', value: '제목' },
-    { disabled: true, label: '내용', value: '내용' },
-    { disabled: true, label: '카테고리', value: '카테고리' },
+    {label: '제목', value: '제목'},
+    {disabled: true, label: '내용', value: '내용'},
+    {disabled: true, label: '카테고리', value: '카테고리'},
   ];
 
   return (
-    <Select
-      defaultValue="제목"
-      onChange={(e) => setSelectedSearchOption(e.value)}
-      options={searchOptions}
-      style={{ minHeight: 40, minWidth: 100, top: 4, width: 'fit-content' }}
-    />
+      <Select
+          defaultValue="제목"
+          onChange={(e) => setSelectedSearchOption(e.value)}
+          options={searchOptions}
+          style={{minHeight: 40, minWidth: 100, top: 4, width: 'fit-content'}}
+      />
   );
 };
 
 const VideoSearchBar: React.FC<VideoSearchBarProps> = ({
-  setSearchResults,
-  stateLoading: { isLoading, setIsLoading },
-  stateValidInput: { isValidInput, setIsValidInput },
-}) => {
+                                                         setSearchResults,
+                                                         stateLoading: {isLoading, setIsLoading},
+                                                         stateValidInput: {
+                                                           isValidInput,
+                                                           setIsValidInput
+                                                         },
+                                                       }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedSearchType, setSelectedSearchType] = useState<string>('제목');
   const [searchAutoComplete, setSearchAutoComplete] =
-    useState<AutoCompleteOption[]>(fakeAutoComplete);
+      useState<AutoCompleteOption[]>(fakeAutoComplete);
 
-  const handleSearchTermChange = ({ target: { value } }) => {
+  const handleSearchTermChange = ({target: {value}}) => {
     setSearchTerm(value);
     setIsValidInput(!!value.length);
   };
@@ -78,40 +82,55 @@ const VideoSearchBar: React.FC<VideoSearchBarProps> = ({
   useEffect(() => {
     if (!debouncedSearchTerm) return;
 
-    const response = getSearchComplete(debouncedSearchTerm);
-    const newAutoComplete: AutoCompleteOption[] = response?.data?.data.titles.map((title) => ({
-      label: title,
-      value: title,
-    }));
-    setSearchAutoComplete(newAutoComplete);
+    getSearchComplete(debouncedSearchTerm)
+        .then((response) => {
+              console.log(response)
+              const newAutoComplete: AutoCompleteOption[] = response?.data?.data.titles.map((title) => (
+                      {
+                        label: title,
+                        value: title,
+                      }
+                  )
+              )
+              setSearchAutoComplete(newAutoComplete);
+            }
+        )
+    // console.table(response?.data);
+    // const newAutoComplete: AutoCompleteOption[] = response?.data?.data.titles.map((title) => {
+    //   return {
+    //     label: title,
+    //     value: title,
+    //   }
+    // });
+    // setSearchAutoComplete(newAutoComplete);
   }, [debouncedSearchTerm]);
   const handleSearch = (value) => {
     if (!value) return;
     setIsLoading(true);
     searchVideos(selectedSearchType, value)
-      .then(({ data }) => setSearchResults(data?.data ?? []))
-      .finally(() => setIsLoading(false));
+    .then(({data}) => setSearchResults(data?.data ?? []))
+    .finally(() => setIsLoading(false));
   };
 
   return (
-    <Space align="center" size={0}>
-      <SelectSearchType setSelectedSearchOption={setSelectedSearchType} />
-      <AutoComplete options={searchAutoComplete}>
-        {selectedSearchType === '제목' && (
-          <Search
-            allowClear
-            enterButton="Search"
-            loading={isLoading}
-            onChange={handleSearchTermChange}
-            onSearch={handleSearch}
-            placeholder="검색어를 입력 해주세요"
-            size="large"
-            status={isValidInput ? '' : 'error'}
-            style={{ maxWidth: 600, minWidth: 400 }}
-          />
-        )}
-      </AutoComplete>
-    </Space>
+      <Space align="center" size={0}>
+        <SelectSearchType setSelectedSearchOption={setSelectedSearchType}/>
+        <AutoComplete options={searchAutoComplete}>
+          {selectedSearchType === '제목' && (
+              <Search
+                  allowClear
+                  enterButton="Search"
+                  loading={isLoading}
+                  onChange={handleSearchTermChange}
+                  onSearch={handleSearch}
+                  placeholder="검색어를 입력 해주세요"
+                  size="large"
+                  status={isValidInput ? '' : 'error'}
+                  style={{maxWidth: 600, minWidth: 400}}
+              />
+          )}
+        </AutoComplete>
+      </Space>
   );
 };
 
